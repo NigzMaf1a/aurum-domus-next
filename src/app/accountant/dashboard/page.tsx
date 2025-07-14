e0ac88845a0e0a9dac7dc9beed8552df4733bd45
Nigel Khasiani <nigelkhasiani@gmail.com>
2025-07-14 10:10:56 +0300

    Accountant module addition

---
+++ src/app/accountant/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+        <div className="container mt-4">
+            <h1 className="mb-4 textColorless">Dashboard</h1>
+            <div className="row" id="cardCont">
+                <div className="col-12 col-sm-6 col-lg-3 mb-4">
+                    <div className="card shadow-sm bg-warning h-100">
+                        <div className="card-body"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Dashboard;
