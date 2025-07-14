ec668c57aa29d23470bf0b6e633b2cc3adf012b3
Nigel Khasiani <nigelkhasiani@gmail.com>
2025-07-14 10:37:10 +0300

    Static Accountant dashboard page

--- src/app/accountant/dashboard/page.tsx
+++ src/app/accountant/dashboard/page.tsx
@@ -2,17 +2,59 @@ import React from 'react';
 
 function Dashboard() {
   return (
-        <div className="container mt-4">
-            <h1 className="mb-4 textColorless">Dashboard</h1>
-            <div className="row" id="cardCont">
-                <div className="col-12 col-sm-6 col-lg-3 mb-4">
-                    <div className="card shadow-sm bg-warning h-100">
-                        <div className="card-body"></div>
-                    </div>
-                </div>
+    <div className="container mt-4">
+      <h1 className="mb-4 textColorless">Dashboard</h1>
+      <div className="row" id="cardCont">
+        {/* Card 1 */}
+        <div className="col-12 col-sm-6 col-lg-3 mb-4">
+          <div className="card shadow-sm bg-warning h-100">
+            <div className="card-body">
+              <h5 className="card-title">Pending</h5>
+              <p className="card-text">Number:</p>
+              <p className="card-text">Amount:</p>
+              <p className="card-text">Link:</p>
             </div>
+          </div>
         </div>
-  )
+
+        {/* Card 2 */}
+        <div className="col-12 col-sm-6 col-lg-3 mb-4">
+          <div className="card shadow-sm h-100 bg-success text-white">
+            <div className="card-body">
+              <h5 className="card-title">Approved</h5>
+              <p className="card-text">Number:</p>
+              <p className="card-text">Amount:</p>
+              <p className="card-text">Link:</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 3 */}
+        <div className="col-12 col-sm-6 col-lg-3 mb-4">
+          <div className="card shadow-sm h-100 bg-danger text-white">
+            <div className="card-body">
+              <h5 className="card-title">Rejected</h5>
+              <p className="card-text">Number:</p>
+              <p className="card-text">Amount:</p>
+              <p className="card-text">Link:</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 4 */}
+        <div className="col-12 col-sm-6 col-lg-3 mb-4">
+          <div className="card shadow-sm h-100 bg-info text-white">
+            <div className="card-body">
+              <h5 className="card-title">Total</h5>
+              <p className="card-text">Number:</p>
+              <p className="card-text">Amount:</p>
+              <p className="card-text">Link:</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
